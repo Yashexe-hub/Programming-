263054c6344d290a96c60d0cd560385adead91a5
Yash Thakur <yashthakur200529@gmail.com>
2026-05-31 23:52:19 +0530

    Add files via upload

---
+++ Compititive/Practice/program5.c
@@ -0,0 +1,31 @@
+/*  
+    ALGORITHM
+
+    START 
+        Accept frist number as no1
+        Accept second number as no2
+        ADD both the numbers
+        Display the result
+    STOP
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+    float i,j,k;
+
+    printf("Enter first number : \n");
+    scanf("%f",&i);
+
+    printf("Enter second number : \n");
+    scanf("%f",&j);
+
+    k = i + j;
+
+    printf("Addition is : %f\n",k);
+
+
+    return 0;
+}
